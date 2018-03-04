ab539df87d3c4b19ae4b454491828923e2f3dd92
wh96 <wh96@kent.ac.uk>
2018-03-04 20:57:51 +0000

    Enable specification of range when testing

--- test.sh
+++ test.sh
@@ -2,7 +2,7 @@
 
 path="../depsolver/tests/seen-"
 
-for i in {2..9}; do
+for i in $(eval echo {$1..$2}); do
   echo "Testing Seen $i"
   ./solve "$path$i/repository.json" "$path$i/initial.json" "$path$i/constraints.json"
   # echo "$path$i/repository.json" "$path$i/initial.json" "$path$i/constraints.json"
